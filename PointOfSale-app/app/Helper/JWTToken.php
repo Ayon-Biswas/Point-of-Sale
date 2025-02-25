19f2c55c38b3ec1a065b69ed9ea680d2543851ec
Ayon Biswas <ayonshirsho@gmail.com>
2025-02-26 01:57:45 +0600

    jwt token class created

---
+++ PointOfSale-app/app/Helper/JWTToken.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Helper;
+
+use Firebase\JWT\JWT;
+
+class JWTToken
+{
+
+    function CreateToken($userEmail):string{
+
+    $key = env('JWT_KEY');
+    $payload = [
+        'iss' =>'laravel_token',
+        'iat'=>time(), //token creation time
+        'exp'=>time()+60*60, //3600s = 1 hour. after an hour token expire
+        'userEmail'=>$userEmail //to understand for whom the token is issued.when token is decoded we'll get the complete payload
+    ];
+
+    return  JWT::encode($payload,$key,'HS256'); //payload, key and encryption algorithm
+    }
+
+    function VerifyToken($token){
+        try{ //if token is tempered or expired then measures are needed. try block will handle token decode and catch will handle errors
+            $key = env('JWT_KEY');
+            $decode = JWT::decode($token,new Key($key,'HS256')); //$token is the token we want to decode. Key is the new JWT object, first parameter is key and second is the encryption algorithm.
+            return $decode->userEmail;
+        }
+        catch(Exception $e){
+            return "unauthorized user";
+        }
+
+    }
+
+}
